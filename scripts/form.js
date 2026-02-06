6c64633263b926497ab7d8e78330c1c59e7bb2f5
CamillaVieiraDevelopment <camillafranciannesrv@gmail.com>
2026-02-06 15:01:08 -0300

    initial commit

--- scripts/form.js
+++ scripts/form.js
@@ -1,10 +1,30 @@
 //list of products
 const products = [
-    { id: "fc-1888", name: "Air conditioning", averagerating: 4.5 },
-    { id: "fc-2050", name: "Gas water", averagerating: 4.7 },
-    { id: "fs-1987", name: "Eletric shower", averagerating: 3.5 },
-    { id: "ac-2000", name: "Solar heating", averagerating: 3.9 },
-    { id: "jj-1969", name: "Automatic door", averagerating: 5.0 }
+    {
+        id: "fc-1888",
+        name: "flux capacitor",
+        averagerating: 4.5
+    },
+    {
+        id: "fc-2050",
+        name: "power laces",
+        averagerating: 4.7
+    },
+    {
+        id: "fs-1987",
+        name: "time circuits",
+        averagerating: 3.5
+    },
+    {
+        id: "ac-2000",
+        name: "low voltage reactor",
+        averagerating: 3.9
+    },
+    {
+        id: "jj-1969",
+        name: "warp equalizer",
+        averagerating: 5.0
+    }
 ];
 
 const select = document.querySelector("#product");
