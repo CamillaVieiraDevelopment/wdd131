54cfc527dbd436c4c3554ae9a61a5d7aaaebb939
CamillaVieiraDevelopment <camillafranciannesrv@gmail.com>
2026-02-13 21:01:07 -0300

    initial commit

--- project/scripts/main.js
+++ project/scripts/main.js
@@ -1,13 +1,13 @@
 /* scripts/main.js - Global Functionality */
 document.addEventListener("DOMContentLoaded", () => {
-    // 1. Function for Footer Dates (Criteria 9 & 10)
+    // 1. Function for Footer Dates
     const updateFooter = () => {
         const yearSpan = document.querySelector("#year");
         const lastMod = document.querySelector("#lastModified");
 
         if (yearSpan) yearSpan.textContent = new Date().getFullYear();
         if (lastMod) {
-            // Template Literal (Criteria 14)
+            // Template Literal
             lastMod.textContent = `Last Modification: ${document.lastModified}`;
         }
     };
